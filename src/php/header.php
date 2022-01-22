992fe63bdef6e7ad8b1145d8afbbb642976b4496
Claudia Lutnik <clutnik.mmt-b2020@fh-salzburg.at.at>
2022-01-22 21:22:52 +0100

    maybe it works now

--- src/php/header.php
+++ src/php/header.php
@@ -4,12 +4,8 @@
     <img src="<?php echo get_template_directory_uri() ?>/images/svg/Logo.svg" alt="Logo">
     <div id="hero_text">
       <h1>Dr. med. univ. Rupert Rettenbacher</h1>
-      <?php
-        $context = Timber::context();
-        $args = get_the_title();
-        $context['hero'] = Timber::get_posts( $args );
-        Timber::render( 'hero.twig', $context );
-      ?>
+      <h3> <?php the_field('herotitle'); ?> </h3>
+      <h3> <?php the_field('herosubtitle'); ?> </h3>
     </div>
   </section>
   <!--navbar-->
